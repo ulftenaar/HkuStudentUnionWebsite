e8df9401152ce28bf00e5ceb064cbad034a88079
ulftenaar <info.pak_ii@proton.me>
2025-04-08 23:49:52 +0200

    second push, first draft of site

---
+++ header.js
@@ -0,0 +1,20 @@
+// footer.js
+window.onload = function() {
+    const HSUHeaderHTML = `
+    <link rel="stylesheet" href="styles.css">
+
+    <body>
+    <div class="header">
+        <a href="index.html" style="flex:2"><img src="Pictures/HKU_StudentUnion_Logo.png" alt="Hsu Logo" Width="100%" Height="100%"></a>
+      <!--  <a target="_blank"href=vrienden.html>About</a> 
+        <a target="_blank"href= UTAblog.html>Articles</a>
+        <a target="_blank"href= UTAlinks.html>Calendar</a>
+        <a target="_blank"href= UTAwerk.html>Become a Member</a>
+        <a target="_blank"href="Muziek/DenkenaanPage/denkenaan.html">Reach out!</a> 
+ -->       </div>
+        
+        </body>
+        `
+        document.body.insertAdjacentHTML('beforebegin', HSUHeaderHTML);
+    }
+    
