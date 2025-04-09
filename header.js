2c7456e5e64a3228a0fcdcdc0d36e59fcd1a7175
ulftenaar <info.pak_ii@proton.me>
2025-04-10 00:29:38 +0200

    new posts, aricles page added

--- header.js
+++ header.js
@@ -6,8 +6,8 @@ window.onload = function() {
     <body>
     <div class="header">
         <a href="index.html" style="flex:2"><img src="Pictures/HKU_StudentUnion_Logo.png" alt="Hsu Logo" Width="100%" Height="100%"></a>
+        <a target="_blank"href= articles.html>articles</a>
       <!--  <a target="_blank"href=vrienden.html>About</a> 
-        <a target="_blank"href= UTAblog.html>Articles</a>
         <a target="_blank"href= UTAlinks.html>Calendar</a>
         <a target="_blank"href= UTAwerk.html>Become a Member</a>
         <a target="_blank"href="Muziek/DenkenaanPage/denkenaan.html">Reach out!</a> 
