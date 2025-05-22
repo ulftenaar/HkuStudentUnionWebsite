a58ce47c6a8466d81bd678044dc8788d555cb496
ulftenaar <info.pak_ii@proton.me>
2025-05-22 14:30:17 +0200

    about page online, calendar page created

--- header.js
+++ header.js
@@ -6,10 +6,12 @@ window.onload = function() {
     <body>
     <div class="header">
         <a href="index.html" style="flex:2"><img src="Pictures/HKU_StudentUnion_Logo.png" alt="Hsu Logo" Width="100%" Height="100%"></a>
-        <a href= articles.html>articles</a>
+        <a href=about.html>About</a>
+        <a href=calendar.html>Calendar</a>
+        <a href= articles.html>Articles</a>
    <!--   <a href= gallery.html>gallery</a> -->
-        <a href=join.html>join</a> 
-        <a href="contact.html">contact</a> 
+        <a href=join.html>Join</a> 
+        <a href="contact.html">Contact</a> 
         </div>
         
         </body>
